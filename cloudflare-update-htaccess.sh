4b13c63e345c6568e6aa7fe9bcc296d5abe432ff
Aleksey Kryachko <41658912+Unixweb-RU@users.noreply.github.com>
2021-12-02 06:29:32 +0300

    Create cloudflare-update-htaccess.sh

---
+++ cloudflare-update-htaccess.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Путь для  генирируемых правил в файле .htaccess
+htaccess_path="/var/www/youruser/data/www/your.site/.htaccess"
+# Путь для настроек основных правил в файле .htaccess
+htaccess_rules="/var/www/youruser/data/www/htaccess"
+echo "# Cloudflare IPv4 Rule Start
+Order Deny,Allow
+Deny from all" > $htaccess_path;
+while read ip ; do echo "allow from $ip" >> $htaccess_path; done <<< "$(curl https://www.cloudflare.com/ips-v4)"
+echo "# End of the IPv4 Cloudflare Rule" >> $htaccess_path;
+echo "# Cloudflare IPv6 Rule Start" >> $htaccess_path;
+while read ip ; do echo "allow from $ip" >> $htaccess_path; done <<< "$(curl https://www.cloudflare.com/ips-v6)"
+echo "# End of the IPv6 Cloudflare Rule" >> $htaccess_path;
+echo "# Other htaccess rules" >> $htaccess_path;
